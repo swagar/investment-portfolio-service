0ccdf553094949b85378e383057da97ef5442acc
Simon Wagar <simon@wagar.de>
2021-11-01 09:14:55 +0100

    initial commit

---
+++ src/main/kotlin/com/vitamin/investment/Application.kt
@@ -0,0 +1,11 @@
+package com.vitamin.investment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InvestmentPortfolioServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<InvestmentPortfolioServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/vitamin/investment/InvestmentPortfolioServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vitamin.investment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InvestmentPortfolioServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
