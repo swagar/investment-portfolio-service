7c184200032156034fa277e7d1e33e8f6c57ad29
Simon Wagar <simon@wagar.de>
2021-11-03 11:53:46 +0100

    added more tests for the core calculator

--- src/test/kotlin/com/vitamin/investment/calculate/CalculatorTest.kt
+++ src/test/kotlin/com/vitamin/investment/calculate/CalculatorTest.kt
@@ -3,6 +3,7 @@ package com.vitamin.investment.calculate
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.vitamin.investment.readValue
 import com.vitamin.investment.stock.History
+import com.vitamin.investment.stock.HistoryEntry
 import com.vitamin.investment.stock.HistoryService
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -16,10 +17,10 @@ class CalculatorTest {
     lateinit var om: ObjectMapper
 
     @Test
-    fun test_a_simple_calulation() {
+    fun `test a simple calculation with real world data`() {
         // given
         val request = Request(
-                startDate = of(2021, 2, 1),
+                startDate = of(2021, 1, 31),
                 endDate = of(2021, 6, 30),
                 monthlyContribution = valueOf(400),
                 loadPortfolio(2)
@@ -34,6 +35,48 @@ class CalculatorTest {
 
         // then
         assert(result.sumContribution == valueOf(2000))
+    }
+
+    @Test
+    fun `test the calculation algo works as expected`() {
+        // given
+        val request = Request(
+                startDate = of(2021, 1, 10),
+                endDate = of(2021, 4, 30),
+                monthlyContribution = valueOf(100),
+                portfolio = listOf(
+                        WeightedStock(weight = valueOf(0.1), ticker = "A"),
+                        WeightedStock(weight = valueOf(0.9), ticker = "B"))
+        )
+        val calculator = Calculator(
+                request = request,
+                historyService = HistoryService(
+                        histories = mapOf(
+                                Pair("A", History(
+                                        listOf(
+                                                HistoryEntry(date = of(2021, 4, 30), close = valueOf(5)),
+                                                HistoryEntry(date = of(2021, 4, 1), close = valueOf(5)),
+                                                HistoryEntry(date = of(2021, 3, 1), close = valueOf(2)),
+                                                HistoryEntry(date = of(2021, 2, 1), close = valueOf(1))
+
+                                        ))),
+                                Pair("B", History(
+                                        listOf(
+                                                HistoryEntry(date = of(2021, 4, 30), close = valueOf(10)),
+                                                HistoryEntry(date = of(2021, 4, 1), close = valueOf(10)),
+                                                HistoryEntry(date = of(2021, 3, 1), close = valueOf(9)),
+                                                HistoryEntry(date = of(2021, 2, 1), close = valueOf(3))
+                                        )))
+                        )
+                )
+        )
+
+        // when
+        val result = calculator.calculateCurrentValue()
+
+        // then
+        assert(result.sumContribution == valueOf(300))
+        assert(result.porfolioValue == valueOf(575).setScale(1))
 
     }
 
